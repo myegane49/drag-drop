23032212c7b9759dfde4cbd1419970440ad00372
Mohammad Ahmady Yeganeh <m.yegane49@gmail.com>
2020-08-01 11:08:10 +0430

    Adds the input class

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,77 @@
-// Code goes here!
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this)
+      return boundFn
+    }
+  }
+  return adjDescriptor
+}
+
+class ProjectInput {
+  templateElement: HTMLTemplateElement
+  hostElement: HTMLDivElement
+  element: HTMLFormElement
+  titleInputElement: HTMLInputElement
+  descriptionInputElement: HTMLInputElement
+  peopleInputElement: HTMLInputElement
+
+  constructor() {
+    this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement
+    this.hostElement = document.getElementById('app')! as HTMLDivElement
+    
+    const importedNode = document.importNode(this.templateElement.content, true)
+    this.element = importedNode.firstElementChild as HTMLFormElement
+    this.element.id = 'user-input'
+    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement
+    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement
+    this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement
+
+    this.configure()
+    this.attach()
+  }
+
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value
+    const enteredDescription = this.descriptionInputElement.value
+    const enteredPeople = this.peopleInputElement.value
+
+    if (enteredTitle.trim().length === 0 || enteredDescription.trim().length === 0 || enteredPeople.trim().length === 0) {
+      alert('invalid input')
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople]
+    }
+  }
+
+  private clearInputs() {
+    this.titleInputElement.value = ''
+    this.descriptionInputElement.value = ''
+    this.peopleInputElement.value = ''
+  }
+
+  @autobind
+  private submitHandler(event: Event) {
+    event.preventDefault()
+    // console.log(this.titleInputElement.value) // this won't point at the class when the method is triggered in the event listener. because this is bound to current target of the event in the method of an event listener
+    const userInput = this.gatherUserInput()
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput
+      console.log(title, desc, people)
+      this.clearInputs()
+    }
+  }
+
+  private configure() {
+    // this.element.addEventListener('submit', this.submitHandler)
+    // this.element.addEventListener('submit', this.submitHandler.bind(this)) // this is the workaround for the this keyword problem
+    this.element.addEventListener('submit', this.submitHandler)
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element)
+  }
+}
+
+const prjInput = new ProjectInput()
